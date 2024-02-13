cb3278da2ea3152fc988cc52aa4f39ea169f88ac
NathanGathukuGachanja <159188824+NathanGathukuGachanja@users.noreply.github.com>
2024-02-13 20:50:50 +0300

    Add files via upload
    Assignment on Fahrenheit

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
 {
-    int a, b, c;
-    printf("Enter the first value : ");
-    scanf("%d" , &a);
-    printf("Enter the second value :");
-    scanf("%d" , &b);
-    c = a + b;
-    printf("%d + %d = %d\n" , a, b, c);
-    return 0;
+    int a,b,c;
+
+
+
+    printf("Enter the starting value:\n");
+    scanf("%d,&a");
+    printf("Enter the increment value:\n");
+    scanf("%d", &b);
+    printf("Enter the ending value:\n");
+    scanf("%d", &c);
+    printf("Fahrenheit-to-Celsius conversion table\n");
+
+    {
+        printf("%4d degrees F = %4d degrees C\n",a,(a-32)*5/9);
+        printf("%4d degrees F = %4d degrees C\n",b,(b-32)*5/9);
+        printf("%4d degrees F = %4d degrees C\n",c,(c-32)*5/9);
+
+    }
+      while(a<=c)
+      return 0;
+
 }
