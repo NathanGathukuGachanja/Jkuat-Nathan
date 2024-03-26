447ed977b45e560c4aa725d401942136f590a5df
NathanGathukuGachanja <159188824+NathanGathukuGachanja@users.noreply.github.com>
2024-03-26 22:14:12 +0300

    Add files via upload

---
+++ Pointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int *ptr;
+
+    ptr = &x;
+
+    printf("The value of x is: %d\n", x);
+    printf("The address of x is: %p\n", &x);
+    printf("The value pointed to by ptr is: %d\n", *ptr);
+    printf("The address stored in ptr is: %p\n", ptr);
+
+    return 0;
+}
